d0cdc0506423ddde5ab3fc483268c65d8823c235
Saduk0 <saduk0@protonmail.com>
2020-02-05 09:13:52 +0300

    Updated to 3.0.3

--- default.nix
+++ default.nix
@@ -4,13 +4,13 @@ with import <nixpkgs> {};
       my_django = python37.pkgs.buildPythonPackage rec {
 
         pname = "Django";
-        version = "3.0.2";
+        version = "3.0.3";
 
         /* disabled = !isPy3k; */
 
         src = python37.pkgs.fetchPypi {
           inherit pname version;
-          sha256 = "8c3575f81e11390893860d97e1e0154c47512f180ea55bd84ce8fa69ba8051ca";
+          sha256 = "2f1ba1db8648484dd5c238fb62504777b7ad090c81c5f1fd8d5eb5ec21b5f283";
         };
 
         propagatedBuildInputs = with python3.pkgs;
@@ -22,7 +22,7 @@ with import <nixpkgs> {};
         preConfigure = ''
           export DJANGO_SETTINGS_MODULE=mysite.settings
         '';
-        
+
         meta = with stdenv.lib; {
           description = "A high-level Python Web framework";
           homepage = https://www.djangoproject.com/;
